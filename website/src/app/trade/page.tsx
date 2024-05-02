88123b6108637e25a9294e413afcc724ed444875
Divyanshu Parihar <divyanshu1447@gmail.com>
2024-05-02 11:42:51 +0530

    changes

--- website/src/app/trade/page.tsx
+++ website/src/app/trade/page.tsx
@@ -5,6 +5,7 @@ export default function handler() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <PlaceOrder />
-    </div>
+    </div> 
+    
   );
 }
